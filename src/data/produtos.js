9227618ec2c3940fa533273b4e08777bf7799fab
Davi Michel Silva <davimichelcomsilva@gmail.com>
2026-09-14 19:10:11 -0300

    feat: adiciona modelagem e listagem estatica de produtos

---
+++ src/data/produtos.js
@@ -0,0 +1,5 @@
+export const produtosIniciais = [
+  { id: 1, nome: "Teclado Mecânico RGB", precoUnitario: 299.90, quantidade: 1 },
+  { id: 2, nome: "Mouse Gamer Wireless", precoUnitario: 189.00, quantidade: 2 },
+  { id: 3, nome: "Headset Estéreo Pro", precoUnitario: 350.00, quantidade: 1 }
+];
