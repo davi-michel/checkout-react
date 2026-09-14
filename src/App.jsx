9da422fe342331079e8eac7149a8c0aef5242a8c
Davi Michel Silva <davimichelcomsilva@gmail.com>
2026-09-14 19:10:03 -0300

    feat: implementa estrutura de rotas globais da aplicacao

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Carrinho } from "./pages/Carrinho";
+import { Pagamento } from "./pages/Pagamento";
+import { Sucesso } from "./pages/Sucesso";
+import { Falha } from "./pages/Falha";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Carrinho />} />
+        <Route path="/pagamento" element={<Pagamento />} />
+        <Route path="/sucesso" element={<Sucesso />} />
+        <Route path="/falha" element={<Falha />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
